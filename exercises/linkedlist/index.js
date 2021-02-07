ef5b7c194803dfd9ec628f21e5f0fc846356d606
Leon Marbukh <leonmarbukh@gmail.com>
2021-02-07 13:15:19 -0800

    more work on ll

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,20 @@ class LinkedList {
     constructor(){
         this.head = null;
     }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+    }
+
+    size(){
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next;
+        }
+        return count;
+    }
 }
 
 module.exports = { Node, LinkedList };
