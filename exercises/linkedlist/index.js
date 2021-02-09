d9968623972db8e10d614beb8d3d61bef784a2c0
Leon Marbukh <leonmarbukh@gmail.com>
2021-02-08 20:02:53 -0800

    more LL tests passed

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -27,6 +27,48 @@ class LinkedList {
         }
         return count;
     }
+
+    getFirst(){
+        return this.head;
+    }
+
+    getLast(){
+        if(!this.head){
+            return null;
+        }
+        let node = this.head;
+        while(node.next){
+            node = node.next
+        }
+        if(node.next === null){
+            return node;
+        }
+    }
+    clear(){
+        this.head = null;
+    }
+    removeFirst(){
+        if(!this.head){
+            return;
+        }
+        this.head = this.head.next;
+    }
+    removeLast(){
+        if(!this.head){
+            return;
+        }
+        if(!this.head.next){
+            this.head = null;
+            return;
+        }
+        let prev = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            prev = node;
+            node = node.next
+        }
+            prev.next = null
+    }
 }
 
 module.exports = { Node, LinkedList };
